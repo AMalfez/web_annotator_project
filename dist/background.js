e12a28d8070f1ecb8df43e277907c8252ca2b5db
AMalfez <a_mansuri@ch.iitr.ac.in>
2024-06-08 12:33:56 +0530

    linked notes ui to url

--- dist/background.js
+++ dist/background.js
@@ -1,32 +1,34 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+    if (changeInfo.status === "complete") {
       chrome.scripting
         .executeScript({
           target: { tabId },
-          files: ["./contentScript.js"]
+          files: ["./contentScript.js"],
+          args: [getCurrentTab()]
         })
         .then(() => {
           console.log("we have injected the content script");
         })
         .catch((err) => console.log(err, "error in background script line 10"));
-  
-      chrome.tabs.sendMessage(
-        tabId,
-        { action: "URL", url: tab.url },
-        function (response) {
-          if (!chrome.runtime.lastError) {
-            console.log(response);
-          } else {
-            console.log(chrome.runtime.lastError, "error line 14");
-          }
-        }
-      );
     }
   });
   
   chrome.tabs.onActivated.addListener(function(activeInfo) {
     if (activeInfo.tabId) {
-      chrome.tabs.sendMessage(activeInfo.tabId, { updateStorage: true });
+      chrome.tabs.sendMessage(activeInfo.tabId, {action:"URL", msg: activeInfo },function (response) {
+        if (!chrome.runtime.lastError) {
+          console.log(response);
+        } else {
+          console.log(chrome.runtime.lastError, "error line 14");
+        }
+      });
     }
-  })
+  });
+
+  async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
   
